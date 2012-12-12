8ea2ca6f49c16ee70e7aa997fee0bcb98497585b
Peter Anderson <peter@pollenizer.com>
2012-12-13 00:55:00 +1100

    removed sudo su

--- ruby.sh
+++ ruby.sh
@@ -1,23 +1,23 @@
 RUBY_VERSION="1.9.3-p327"
 
-sudo su
+
 apt-get update
 
 # setup deploy user
-useradd deploy
+sudo useradd deploy
 
 # install git
-apt-get install git
+sudo apt-get install git
 
 # install ruby dependancies
-apt-get install build-essential openssl libreadline6 libreadline6-dev git-core zlib1g zlib1g-dev libssl-dev libyaml-dev
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev git-core zlib1g zlib1g-dev libssl-dev libyaml-dev
 
 # install rbenv
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+sudo echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+sudo echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
-exec $SHELL
+sudo exec $SHELL
 
 # install ruby build and latest ruby
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
