d3e2d6f54586d948ece804f445b5928e517971cf
Peter Anderson <peter@pollenizer.com>
2012-12-13 01:17:28 +1100

    more changes

--- ruby.sh
+++ ruby.sh
@@ -1,5 +1,6 @@
 RUBY_VERSION="1.9.3-p327"
 
+echo Installing ruby $RUBY_VERSION...
 
 apt-get update
 
@@ -10,18 +11,20 @@ sudo useradd deploy
 sudo apt-get install git
 
 # install ruby dependancies
-sudo apt-get install build-essential openssl libreadline6 libreadline6-dev git-core zlib1g zlib1g-dev libssl-dev libyaml-dev
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev git-core zlib1g zlib1g-dev libssl-dev libyaml-dev 
+
+# install mysql client
+sudo apt-get installmysql-client libmysqlclient-dev
 
 # install rbenv
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
-exec $SHELL
-source ~/.bash_profile
-
 # install ruby build and latest ruby
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+source ~/.bash_profile
 
 rbenv install $RUBY_VERSION
 rbenv rehash
